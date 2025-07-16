e4c884fc142043c3a3c26c7249eb60994b251178
Milton Tavares <tonny2@tony.>
2025-07-16 17:08:01 -0400

    very lost

--- input-password-app/src/components/PasswordInput.jsx
+++ input-password-app/src/components/PasswordInput.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function PasswordInput() {
     function handleChange() {
         console.log("Entering password...");
--- input-password-app/src/components/SubmitButton.jsx
+++ input-password-app/src/components/SubmitButton.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+
+
 function SubmitButton() {
     function handleEnter() {
         console.log("Mouse Entering");
